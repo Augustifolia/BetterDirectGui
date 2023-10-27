2bee8e0dfcb1ca4120857b380b658207af897c1d
Augustifolia <august.malmdahl@gmail.com>
2023-10-27 15:01:35 +0200

    Some improvements to themability and other various changes

--- BetterDirectGui/__init__.py
+++ BetterDirectGui/__init__.py
@@ -10,12 +10,14 @@ from .DirectGuiBase import DirectGuiWidget
 __all__ = ["init", "DirectGui", "DirectGuiBase", "GuiController"]
 
 
-def init(base_np=None, respect_sortOrder=True, do_bug_fixes=False):
+def init(base_np=None, respect_sortOrder=True, do_bug_fixes=False, theme=None, do_keyboard_navigation=True):
     """Initialize GuiController and setup keyboard navigation.
 
     :param base_np: The NodePath that keyboard navigation should start from.
     :param respect_sortOrder: If True: navigation will take into account the sort order of the gui elements
      when selecting the next element to jump to.
     :param do_bug_fixes: Changes some buggy behaviour in DirectGui.
+    :param theme: The global theme used by all elements that are children of base_np.
+    :param do_keyboard_navigation: Chose weather keyboard navigation is enabled.
     """
-    GuiController(base_np, respect_sortOrder, do_bug_fixes)
+    GuiController(base_np, respect_sortOrder, do_bug_fixes, theme, do_keyboard_navigation)
